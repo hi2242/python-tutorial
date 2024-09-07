9afda0d8b8f40db33056295d72b05856fe9171b5
hi2242 <137996885+hi2242@users.noreply.github.com>
2024-09-07 14:49:45 +0900

    Update 수 찾기.py

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -3,7 +3,9 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-
+# set을 이용하면 리스트 내에 중복도 제거 할 수 있고 해시 테이블이라는
+# 자료구조를 기반으로 동작하기 때문에 원소를 찾을 때 O(1)만큼만 시간이 걸린다.
+# 최악의 경우는 시간 복잡도가 O(N)이 될 수도 있다.
 rowA = set(map(int, input().split()))
 
 M = int(input())
