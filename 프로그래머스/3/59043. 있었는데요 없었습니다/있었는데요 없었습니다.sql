7b48181691705fad30381552bd1571a69d6e3804
hi2242 <137996885+hi2242@users.noreply.github.com>
2025-02-20 19:35:29 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,21 @@
+-- 코드를 입력하세요
+-- 1. ANIMAL_INS (테이블) : 보호소에 들어온 동물 정보
+-- ANIMAL_ID (동물 ID), ANIMAL_TYPE (생물 종), DATETIME (보호 시작일)
+-- , INTAKE_CONDITION (보호 시작 시 상태), NAME (이름), SEX_UPON_INTAKE (성별 및 중성화 여부)
+
+-- 2. ANIMAL_OUTS (테이블) : 보호소에서 나간 동물 정보
+-- ANIMAL_ID (동물 ID), ANIMAL_TYPE (생물 종), DATETIME (입양일)
+-- , NAME (이름), SEX_UPON_INTAKE (성별 및 중성화 여부)
+# ANIMAL_OUTS의 ANIMAL_ID는 ANIMAL_INS의 ANIMAL_ID 외래 키이다.
+
+-- 문제
+# ANIMAL_INS.DATETIME보다 ANIMAL_OUTS.DATETIME이 더 빠른 ANIMAL_ID 조회
+# ANIMAL_INS.DATETIME 기준으로 오름차순 정렬
+
+SELECT I.ANIMAL_ID, I.NAME
+FROM ANIMAL_INS I
+LEFT JOIN ANIMAL_OUTS O
+ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.DATETIME > O.DATETIME
+ORDER BY I.DATETIME
+;
